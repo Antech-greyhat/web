d8379634521af15449cec310775ea195540421d7
Antony Mwendwa <antonymwendwa408@gmail.com>
2025-11-26 10:31:04 +0300

    Log button click event to console

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 function clicked(){
   alert(" SEND MONEY: 0714452396 ");
+  console.log("Button clicked! send money 0714452396")
 }
 function submitted(){
   alert(" details submitted succefully ");
